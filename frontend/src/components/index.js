1c189a28e623685de501a58bc17a2aaba16a3d57
Stefan Stefanovic <stefdev@Stefans-MacBook-Pro.local>
2022-12-04 20:23:49 +0100

    added axios and refactored composer, added env

--- frontend/src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Layout } from './ui/Layout';
-export { default as Navbar } from './ui/Navbar';
-export { default as Footer } from './ui/Footer';
-export { default as PageWrapper } from './ui/PageWrapper';
